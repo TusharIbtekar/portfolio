91b6fab7465129597ab215605da00d7865d124cc
TusharIbtekar <ibtekar8@gmail.com>
2023-08-23 03:05:35 +0600

    fixed tailwind issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,16 @@ export default function HomePage() {
   
 
   return (
-    <div>
-      here
+    <div className="min-h-screen flex items-center justify-center p-12">
+      <div className="max-w-md sm:max-w-lg md:sm:max-w-2xl lg:sm:max-w-3xl w-full space-y-8 text-center">
+        Tushar Ibtekar
+      </div>
+      <div className="">
+        Software Engineer
+      </div>
+      <div className="text-sky-500 dark:text-sky-400">
+        Sarah Dayan
+      </div>
     </div>
   )
 }
