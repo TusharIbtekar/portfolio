281a486a562cf9c3c5c7d29607778b33c225e413
TusharIbtekar <ibtekar8@gmail.com>
2023-08-26 23:03:46 +0600

    add navbar hovering animation

--- src/components/Navbar/NavigationBar.component.tsx
+++ src/components/Navbar/NavigationBar.component.tsx
@@ -1,13 +1,19 @@
 import { ACTIONS } from "@/lib";
 import Link from "next/link";
-
+import { motion } from "framer-motion";
 export default function NavigationBar() {
   return (
     <div className="space-x-5 sm:flex">
       {ACTIONS.map((action, index) => {
         return (
-          <Link key={index} className="ml-2" href={action.href || "/"}>
-            {action.text}
+          <Link
+            key={index}
+            className="horizontal-underline text-base hover:horizontal-underline"
+            href={action.href || "/"}
+          >
+            <span className="tracking-wide text-gray-900 dark:text-gray-100">
+              {action.text}
+            </span>
           </Link>
         );
       })}
