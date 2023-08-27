85f3e6c38664125a64af00594356ae5d75b6d5a6
TusharIbtekar <ibtekar8@gmail.com>
2023-08-28 04:07:57 +0600

    setup timeline page

--- src/lib/navigation.tsx
+++ src/lib/navigation.tsx
@@ -4,19 +4,19 @@ import { Library, Github, FolderGit2, ShipWheel, Linkedin } from "lucide-react";
 export const ACTIONS: Array<NavigationItem> = [
   {
     type: NavigationItemType.LINK,
-    href: "#",
+    href: "/blog",
     icon: <Library />,
     text: "Blog",
   },
   {
     type: NavigationItemType.LINK,
-    href: "#",
+    href: "/projects",
     icon: <FolderGit2 />,
     text: "Projects",
   },
   {
     type: NavigationItemType.LINK,
-    href: "#",
+    href: "/timeline",
     icon: <ShipWheel />,
     text: "Timeline",
   },
