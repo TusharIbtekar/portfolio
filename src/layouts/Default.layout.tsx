b17a5b4a5615fa8b1bc6fe30c6267096918e0826
TusharIbtekar <ibtekar8@gmail.com>
2023-08-29 03:43:27 +0600

    add page transitions

--- src/layouts/Default.layout.tsx
+++ src/layouts/Default.layout.tsx
@@ -1,15 +1,27 @@
-import { Navbar } from "@/components";
 import { PropsWithChildren } from "react";
+import { motion } from "framer-motion";
+import { Navbar } from "@/components";
 
-export function DefaultLayout({
-  children
-}: PropsWithChildren): JSX.Element {
-  return(
+export function DefaultLayout({ children }: PropsWithChildren): JSX.Element {
+  const variants = {
+    hidden: { opacity: 0, x: -200 },
+    enter: { opacity: 1, x: 0 },
+    exit: { opacity: 0, x: 0 },
+  };
+  return (
     <>
-      <Navbar.Standard />
-      <main className="flex flex-col justify-center px-8">
-        {children}
-      </main>
+      <motion.main
+        data-scroll
+        className="mb-auto"
+        initial="hidden"
+        animate="enter"
+        exit="exit"
+        variants={variants}
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+      >
+        <Navbar.Standard />
+        <div className="flex flex-col justify-center px-8">{children}</div>
+      </motion.main>
     </>
-  )
+  );
 }
