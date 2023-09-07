4d0f78a218ba1425498eccc69aae347a7bc07e9f
TusharIbtekar <ibtekar8@gmail.com>
2023-09-07 17:04:29 +0600

    fix: footer color in light theme

--- src/components/Footer/Footer.component.tsx
+++ src/components/Footer/Footer.component.tsx
@@ -3,12 +3,14 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <div className="absolute flex flex-row bottom-12 right-40 w-auto">
+    <div className="absolute flex flex-row justify-between bottom-12 right-40 w-20">
       {ACTIONS.map((action, index) => {
         return (
           action.slot === "footer" && (
             <Link key={index} className="" href={action.href || "/"}>
-              <span className="px-12 dark:text-blue-400">{action.icon}</span>
+              <span className="dark:text-blue-400 dark:hover:text-gray-500 text-gray-500 hover:text-blue-400">
+                {action.icon}
+              </span>
             </Link>
           )
         );
