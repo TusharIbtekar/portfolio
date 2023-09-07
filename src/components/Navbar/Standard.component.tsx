774b554c0f903cd7b8ab874f0730dbbdc4e2948d
TusharIbtekar <ibtekar8@gmail.com>
2023-09-07 16:15:36 +0600

    feat: no nav on landing page

--- src/components/Navbar/Standard.component.tsx
+++ src/components/Navbar/Standard.component.tsx
@@ -3,14 +3,14 @@ import NavigationBar from "./NavigationBar.component";
 import { usePathname } from "next/navigation";
 
 export function Standard(): JSX.Element {
-  // const isHome = usePathname() === "/" ? true : false;
+  const isHome = usePathname() === "/" ? true : false;
   return (
     <nav className="fixed w-full bg-transparent py-5 md:py-10">
       <div className="mx-auto flex max-w-5xl items-center justify-between">
-        {/* <div>{!isHome && <NavigationBar />}</div> */}
-        <div>
+        <div>{!isHome && <NavigationBar />}</div>
+        {/* <div>
           <NavigationBar />
-        </div>
+        </div> */}
         <div className="flex items-center">
           <ThemeSwitcher />
         </div>
