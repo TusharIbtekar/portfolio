446805fb88f838257feb71db03a24db406f565ff
TusharIbtekar <ibtekar8@gmail.com>
2023-09-10 13:49:05 +0600

    fix: card size

--- src/components/Projects/ProjectCard.component.tsx
+++ src/components/Projects/ProjectCard.component.tsx
@@ -5,10 +5,10 @@ import Link from "next/link";
 
 export function ProjectCard({ projects }: Projects): JSX.Element {
   return (
-    <div>
+    <div className="w-full">
       {projects.map((project, index) => {
         return (
-          <div className="primary-morphs px-7 py-4 w-full mt-10">
+          <div className="primary-morphs px-7 py-4 mt-10">
             <div className="flex justify-between items-center">
               <h2 className="text-lg md:text-xl font-bold text-primary-color">
                 {project.name}
--- src/components/Timeline/ExperienceCard.component.tsx
+++ src/components/Timeline/ExperienceCard.component.tsx
@@ -3,10 +3,10 @@ import { Tag } from "@/components/Tags";
 
 export function ExperienceCard({ experiences }: Experiences): JSX.Element {
   return (
-    <div className="primary-morphs px-7 py-4 w-full mt-10">
+    <div className="w-full">
       {experiences.map((experience, index) => {
         return (
-          <>
+          <div className="primary-morphs px-7 py-4 mt-10">
             <div className="flex justify-between items-center">
               <h2 className="text-lg md:text-xl font-bold text-primary-color">
                 {experience.title}
@@ -17,7 +17,7 @@ export function ExperienceCard({ experiences }: Experiences): JSX.Element {
             </div>
             <p className="text-sm">{experience.role}</p>
             <Tag skills={experience.skills} />
-          </>
+          </div>
         );
       })}
     </div>
